ad0dd69288a8fd2ce40b44da2a12666cbe60478a
Yorokobii <lennonaddicts@hotmail.fr>
2017-03-23 16:44:40 +0100

    zef

--- js/projet.js
+++ js/projet.js
@@ -13,8 +13,10 @@ function init(){
 	canvas = document.getElementById("ProjectCanvas");
 
 	cwidth = window.innerWidth/5;
+	WorldObject.cwidth = window.innerWidth/5;
 	canvas.width = cwidth;
 	cheight = window.innerHeight;
+	WorldObject.cheight = window.innerHeight;
 	canvas.height = cheight;
 	canvas.style.marginLeft = ((window.innerWidth/2) - (cwidth/2)) + "px";
 
