e43a0ff5edb3111d7f0c176917dd25a35686907d
Yorokobii <lennonaddicts@hotmail.fr>
2017-04-06 10:40:01 +0200

    ajout de briques dans le main

--- js/projet.js
+++ js/projet.js
@@ -42,9 +42,9 @@ function init(){
 	WorldObject.pjs = [];
 
 	brick.push(new Brick(3, 2, 2));
-	/*brick.push(new Brick(4, 1, 3));
+	brick.push(new Brick(4, 1, 3));
 	brick.push(new Brick(5, 4, 5));
-	brick.push(new Brick(2, 2, 5));*/
+	brick.push(new Brick(2, 2, 5));
 	for(var i=0; i<brick.length; ++i)
 		brick[i].draw(stage);
 
